afbdf56626b5cb8d4aadffd5122e08dcac74b6c6
Umar Khan <khanumar0108@gmail.com>
2023-06-12 18:26:25 +0530

    Initialize new branch

---
+++ contracts/FinalAssessment.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
+contract MyToken {
+
+    // public variables here
+    string public constant TOKEN_NAME = "Umar Coin";
+    string public constant TOKEN_ABBRIVIATION = "UC";
+    uint public  tokenSupply = 5000000;
+
+    // mapping variable here
+
+    mapping(address=>uint) public balance;
+
+    // mint function
+    function mint(address _address, uint _supply) public {
+
+        balance[_address] += _supply;
+        tokenSupply += _supply;
+
+    }
+
+    // burn function
+    function burn(address _address, uint _supply) public returns (string memory) {
+
+        uint bal = balance[_address];
+        if(_supply>bal){
+            return "Can't burn as you don't have enough supply";
+        }
+
+        tokenSupply -= _supply;
+        balance[_address] -= _supply;
+
+        return "Your supply has been burnt successfully";
+
+    }
+
+}
